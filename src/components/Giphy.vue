7fbcdab3a1a860f16ebddd176708245959283709
mateusragazzi <mateusragazzi.b@gmail.com>
2020-10-05 22:23:14 -0400

    Adicionando Vuetify e componente do Giphy

---
+++ src/components/Giphy.vue
@@ -0,0 +1,18 @@
+<template>
+    <!-- ITERAR OS DADOS PARA MOSTRAR OS GIPHY's --->
+    <h1>Giphy!</h1>
+</template>
+
+<script>
+export default {
+  name: 'Giphy',
+  props: {
+      // Não tem, a princípio
+  },
+  data: () => ({
+    /** 
+     * @todo trazer os dados do Giphy
+     */
+  }),
+}
+</script>
